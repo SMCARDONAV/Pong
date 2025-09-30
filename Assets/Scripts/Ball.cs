e5f2b95ffd2dec98d0dc2f8150d9bddea476cc8c
SMCARDONAV <92750321+SMCARDONAV@users.noreply.github.com>
2025-09-29 19:38:52 -0500

    Configuración inicial con movimiento de la pelota y rebotes en los bordes

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,42 @@
 using UnityEngine;
 
-public class Pad
+public class Ball : MonoBehaviour
 {
+  
+    float
+        constantXSpeed = 8f,
+        constantYSpeed = 10f,
+        extents = 0.5f;
+
+    public float Extents => extents;
+	
+	public Vector2 Position => position;
+
+    Vector2 position, velocity;
+
+    public void UpdateVisualization() =>
+        transform.localPosition = new Vector3(position.x, 0f, position.y);
+
+    public void Move() => position += velocity * Time.deltaTime;
     
+    public void StartNewGame ()
+	{
+		position = Vector2.zero;
+		UpdateVisualization();
+		velocity = new Vector2(constantXSpeed, -constantYSpeed);
+	}
+
+    	public void BounceX (float boundary)
+	{
+		position.x = 2f * boundary - position.x;
+		velocity.x = -velocity.x;
+	}
+
+	public void BounceY (float boundary)
+	{
+		position.y = 2f * boundary - position.y;
+		velocity.y = -velocity.y;
+	}
 }
+
+
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,6 +1,3 @@
 using UnityEngine;
 
-public class Paddle
-{
-    
-}
+public class Paddle : MonoBehaviour {}
